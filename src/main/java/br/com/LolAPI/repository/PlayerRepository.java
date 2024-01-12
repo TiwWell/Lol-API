7e4f1656b158ff480baa8089c2e6d2f629345f56
TiwWell <wellissonrodrigues12@gmail.com>
2024-01-12 20:33:41 -0300

    Correction of aplication.YML

--- src/main/java/br/com/LolAPI/repository/PlayerRepository.java
+++ src/main/java/br/com/LolAPI/repository/PlayerRepository.java
@@ -3,6 +3,8 @@ package br.com.LolAPI.repository;
 import br.com.LolAPI.entity.PlayerEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
 
 }
